8429b55c0d6446650569bdf35a2dd7c7e4d3b2d3
isg <jjang9b@gmail.com>
2017-06-15 23:24:16 +0900

    google-signin

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,8 +6,20 @@ import {
 import { StyleProvider, Button, Icon, Text } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 import Entypo from 'react-native-vector-icons/Entypo';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 
 export default class Nav extends Component {
+  _signIn() {
+    GoogleSignin.signIn()
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((err) => {
+      console.log('WRONG SIGNIN', err);
+    })
+    .done();
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -15,6 +27,11 @@ export default class Nav extends Component {
             <Entypo name='fingerprint' size={20} color='white' />
             <Text> SET</Text>
           </Button>
+          <GoogleSigninButton
+            style={{width: 48, height: 48}}
+            size={GoogleSigninButton.Size.Icon}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={() => { this._signIn(); }} />
       </View>
     );
   }
